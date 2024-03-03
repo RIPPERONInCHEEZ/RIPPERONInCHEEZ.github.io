35f0311df7993928a80b43255748e453ccca5731
cheez, RIPPERONInCHEEZ, kdick <kdick518@icloud.com>
2024-03-03 10:08:35 -0500

    moar mobile testing

--- docs/Scripts/navhide.js
+++ docs/Scripts/navhide.js
@@ -1,14 +1,16 @@
-// JavaScript code to toggle the .nav-hidden class on scroll
 let lastScrollTop = 0;
 const navbar = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  console.log("Scroll top:", scrollTop);
   if (scrollTop > lastScrollTop) {
     // User is scrolling down
+    console.log("Scrolling down");
     navbar.classList.add("nav-hidden");
   } else {
     // User is scrolling up
+    console.log("Scrolling up");
     navbar.classList.remove("nav-hidden");
   }
   lastScrollTop = scrollTop;
