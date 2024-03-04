ef9bdbd32c92ff276e38d8750891a5cacaed466f
cheez, RIPPERONInCHEEZ, kdick <kdick518@icloud.com>
2024-03-04 02:40:51 -0500

    Merge branch 'main' of https://github.com/RIPPERONInCHEEZ/RIPPERONInCHEEZ.github.io

---
+++ Assets/Scripts/detectmobile.js
@@ -0,0 +1,27 @@
+// Function to detect if the device is a mobile device
+function isMobileDevice() {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
+
+// Function to apply CSS based on device type
+function applyCSS() {
+  const head = document.getElementsByTagName("head")[0];
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+
+  if (isMobileDevice()) {
+    link.href = "Styles/mobilestyle.css";
+    console.log("Mobile device detected. Applying mobile.css style.");
+  } else {
+    link.href = "Styles/overall.css";
+    console.log("Desktop device detected. Applying overall.css style.");
+  }
+
+  head.appendChild(link);
+}
+
+// Apply CSS when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", applyCSS);
